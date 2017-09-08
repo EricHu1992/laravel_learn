b1b7d359bcab1908b18bcaf727b426ffc8e0d068
hubin <hb5201314>
2017-09-08 14:40:44 +0800

    sql

--- demo.sql
+++ demo.sql
@@ -1,26 +1,15 @@
-# Host: 127.0.0.1  (Version: 5.6.34-log)
-# Date: 2017-09-08 14:17:08
-# Generator: MySQL-Front 5.3  (Build 4.269)
+CREATE TABLE IF NOT EXISTS student(
 
-/*!40101 SET NAMES gb2312 */;
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
 
-#
-# Structure for table "student"
-#
+    `name` VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'ÂßìÂêç',
 
-DROP TABLE IF EXISTS `student`;
-CREATE TABLE `student` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '–’√˚',
-  `age` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT 'ƒÍ¡‰',
-  `sex` tinyint(3) unsigned NOT NULL DEFAULT '10' COMMENT '–‘±',
-  `created_at` int(11) NOT NULL DEFAULT '0' COMMENT '–¬‘ˆ ±º‰',
-  `updated_at` int(11) NOT NULL DEFAULT '0' COMMENT '–ﬁ∏ƒ ±º‰',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1013 DEFAULT CHARSET=utf8 COMMENT='—ß…˙±Ì';
+    `age` TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Âπ¥ÈæÑ',
 
-#
-# Data for table "student"
-#
+    `sex` TINYINT UNSIGNED NOT NULL DEFAULT 10 COMMENT 'ÊÄßÂà´',
+    
+    `created_at` INT NOT NULL DEFAULT 0 COMMENT 'Êñ∞Â¢ûÊó∂Èó¥',
 
-INSERT INTO `student` VALUES (1001,'student0',14,10,0,0),(1002,'student1',15,10,0,0),(1003,'student2',16,10,0,0),(1004,'student3',17,10,0,0),(1005,'student4',18,10,0,0),(1006,'student5',19,10,0,0),(1007,'student6',20,10,0,0),(1008,'student7',21,10,0,0),(1009,'student8',22,10,0,0),(1010,'student9',23,10,0,0),(1011,'sean',18,10,0,0),(1012,'eric',18,10,1504678332,1504678332);
+    `updated_at` INT NOT NULL DEFAULT 0 COMMENT '‰øÆÊîπÊó∂Èó¥'
+
+)ENGINE=INNODB DEFAULT CHARSET = UTF8 AUTO_INCREMENT=1001 COMMENT='Â≠¶ÁîüË°®';
